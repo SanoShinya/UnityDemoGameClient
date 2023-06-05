bdbd352d4f047630ed711c5d244fb1a1f6012e79
SanoShinya <shinya.sano.m@gmail.com>
2023-06-05 11:25:41 +0900

    Arborで遷移するEntryState, LoadingState, BattleStateを追加

---
+++ Assets/Scripts/State/LoadingState.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using Arbor;
+
+[AddComponentMenu("")]
+public class LoadingState : StateBehaviour {
+
+	public StateLink _nextState;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+
+	// Use this for awake state
+	public override void OnStateAwake() {
+	}
+
+	// Use this for enter state
+	public override void OnStateBegin() {
+	}
+
+	// Use this for exit state
+	public override void OnStateEnd() {
+	}
+	
+	// OnStateUpdate is called once per frame
+	public override void OnStateUpdate() {
+		if (Input.GetKeyDown(KeyCode.Space)) 
+		{
+        	Transition(_nextState);
+    	}
+	}
+
+	// OnStateLateUpdate is called once per frame, after Update has finished.
+	public override void OnStateLateUpdate() {
+	}
+}
